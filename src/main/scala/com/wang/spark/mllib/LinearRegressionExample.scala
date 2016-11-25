017c856d57fefd031076afd5c5c6226f4bd85f9c
Jeff W <jeffwji@yahoo.com>
2016-11-25 16:21:08 -0500

    change price

--- src/main/scala/com/wang/spark/mllib/LinearRegressionExample.scala
+++ src/main/scala/com/wang/spark/mllib/LinearRegressionExample.scala
@@ -33,6 +33,15 @@ class LinearRegressionExample {
   val sparkconf = new SparkConf().setAppName("LinearRegressionExample").setMaster("local")
   val sc = new SparkContext(sparkconf)
 
+  /**
+    * 训练用的 Seed file 文件格式可以是例如（房屋面，价格）。在本例中故意将价格等于面积x1000是为了方便验证结果。
+    * 因为最终我们要用差方（R^2）来检验结果，但是因为没有真实数据，所以假定一个价格计算公式。
+    *
+    * 预测文件输入房屋面积，得到价格输出。然后用假定的的计算公式获得所谓的真实价格，然后来得到R^2
+    *
+    * @param seedFile
+    * @return
+    */
   def training(seedFile: String): LinearRegressionModel = {
     val data = sc.textFile(seedFile)
     val parsedData = data.map { line =>
@@ -51,7 +60,7 @@ class LinearRegressionExample {
     val data = sc.textFile(seedFile)
     val valuesAndPreds = data.map { line =>
       val prediction = model.predict(Vectors.dense(line.split(' ').map(_.toDouble)))
-      (line.toDouble, prediction)
+      (line.toDouble*1000, prediction)
     }.cache()
 
     val MSE = valuesAndPreds.map{
